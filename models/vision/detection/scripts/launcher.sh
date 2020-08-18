e743229be7c7358046536fe47534f80a80fded1f
Ubuntu <ubuntu@ip-192-168-78-20.ec2.internal>
2020-08-18 06:00:04 +0000

    Added 8 node training config

--- models/vision/detection/scripts/launcher.sh
+++ models/vision/detection/scripts/launcher.sh
@@ -1,3 +1,4 @@
+set -ex
 conda_path=/shared/conda
 source $conda_path/etc/profile.d/conda.sh
 conda activate base
--- models/vision/detection/scripts/train_ec2_8_node.sh
+++ models/vision/detection/scripts/train_ec2_8_node.sh
@@ -12,20 +12,22 @@ echo ""
 cd /shared/deep-learning-models/models/vision/detection
 export PYTHONPATH=${PYTHONPATH}:${PWD}
 
-/opt/amazon/openmpi/bin/mpirun -np ${NUM_GPU} \
+/opt/amazon/openmpi/bin/mpirun \
 --allow-run-as-root \
 --mca plm_rsh_no_tree_spawn 1 \
 --tag-output \
+--hostfile /shared/hosts \
+-N 8 \
 -mca btl_tcp_if_exclude lo,docker0 \
 -x LD_LIBRARY_PATH \
 -x PATH \
 -x NCCL_SOCKET_IFNAME=^docker0,lo \
 -x NCCL_DEBUG=INFO \
 -x TF_CUDNN_USE_AUTOTUNE=0 \
+-x PYTHONPATH \
 --oversubscribe \
 bash /shared/deep-learning-models/models/vision/detection/scripts/launcher.sh \
 python tools/train.py \
 --config ${TRAIN_CFG} \
 --autoscale-lr \
 --amp
-
