124c50f4505b0f548aa06772cf5b81d2106e87bd
Ziyi Mu <ziyi.mu@columbia.edu>
2021-04-15 15:12:58 -0700

    Add nccl req file (#12)

---
+++ models/nlp/albert/requirements.sh
@@ -0,0 +1,10 @@
+pip install --no-cache-dir \
+        scikit-learn==0.23.1 \
+        wandb==0.9.1 \
+        tensorflow-addons \
+        colorama==0.4.3 \
+        pandas \
+        apache_beam \
+        pyarrow==0.16 \
+        git+https://github.com/HerringForks/transformers.git@nccl \
+        git+https://github.com/huggingface/nlp.git@703b761
